e454b319844f901c0c36ac8587207b30740b0e76
Erik Gerrits <ghrgerrits@gmail.com>
2018-03-25 13:04:08 +0200

    Added dotenv support.

--- cmd/unwind/main.go
+++ cmd/unwind/main.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/Stirreg/unwind"
 	"github.com/Stirreg/unwind/json"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	http.HandleFunc("/", func(responseWriter http.ResponseWriter, request *http.Request) {
 		dateTime, err := time.Parse("2006-01-02", request.URL.Path[1:])
 		if err != nil {
@@ -21,5 +28,5 @@ func main() {
 		fmt.Fprint(responseWriter, entry)
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(os.Getenv("APP_PORT"), nil))
 }
